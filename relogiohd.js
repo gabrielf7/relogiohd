af6301aac69a25d061514c4673564d8768b25e2c
JG <diasusb@hotmail.com>
2022-05-15 01:41:05 -0300

    Add: Segundo Estilo de Layout

--- relogiohd.js
+++ relogiohd.js
@@ -4,6 +4,8 @@ const semana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Qui
 */
 
 function CarregarRelogio() {
+  // Pegar Class do HTML
+  let selectClass = (element) => window.document.querySelectorAll(element);
   // Pegar os seletores do relógio (Hora, Minuto e Segundo)
   let hora = window.document.querySelector("[data-ponteiro-hora]");
   let minuto = window.document.querySelector("[data-ponteiro-minuto]");
@@ -36,10 +38,46 @@ function CarregarRelogio() {
     );
   }
 
+  // Estilo da Tag SPAN
+  function EstiloBottomSpan(elemento01, elemento02) {
+    selectClass(elemento01).forEach(element => element.setAttribute("class", `${elemento02}`));
+  }
+
+  // Imagem do Fundo da Página
+  function InserirImagemDeFundo() {
+    let hora = dataAtual().getHours();
+    let linkImgFundo = window.document.getElementById("linkImgFundo");
+    let classExiste = !!document.getElementsByClassName("bottomSpanNoite");
+    let imgBody = window.document.getElementById("body-bg");
+    if(hora < 6) {
+      imgBody.setAttribute("class", "madrugada");
+      EstiloBottomSpan(".bottomSpanDia", "bottomSpanNoite");
+      linkImgFundo.setAttribute("href", "https://unsplash.com/photos/OxU08SFhPbI");
+      linkImgFundo.textContent = "Image background created by Daniel Chekalov - Unsplash";
+    } else if(hora < 13) {
+      imgBody.setAttribute("class", "manha");
+      classExiste === true ? EstiloBottomSpan(".bottomSpanNoite", "bottomSpanDia") : "";
+      linkImgFundo.setAttribute("href", "https://unsplash.com/photos/bcLE7reXFLM");
+      linkImgFundo.textContent = "Image background created by Toa Heftiba - Unsplash";
+    } else if(hora < 18) {
+      imgBody.setAttribute("class", "tarde");
+      classExiste === true ? EstiloBottomSpan(".bottomSpanNoite", "bottomSpanDia") : "";
+      linkImgFundo.setAttribute("href", "https://pixabay.com/pt/illustrations/paisagem-ilustra%c3%a7%c3%a3o-outono-mulher-4572804/");
+      linkImgFundo.textContent = "Image background created by aalmeidah - Pixabay";
+    } else if(hora < 24) {
+      imgBody.setAttribute("class", "noite");
+      EstiloBottomSpan(".bottomSpanDia", "bottomSpanNoite");
+      linkImgFundo.setAttribute("href", "https://unsplash.com/photos/1SiXS0xQHTA");
+      linkImgFundo.textContent = "Image background created by Lili Kovac - Unsplash";
+    } else {
+      window.document.body.style.backgroundColor = "#674A70";
+    }
+  }
+
   // Informar o Nome do Fuso Horário
   function NomeDoFusoHorario() {
     let nomeFH = dataAtual().toLocaleDateString("pt-br", nomeDoFusoHorario).slice(2);
-    document.getElementById("nomeDoHorario").innerHTML = `${nomeFH}`;
+    document.getElementById("nomeDoFusoHorario").innerHTML = `${nomeFH}`;
   }
 
   // Descobrir Data e Horario //
@@ -65,7 +103,13 @@ function CarregarRelogio() {
   function TempoDoRelogio() {
     nomeDoFusoHorario.timeZoneName = "short";
     let pNomeDoFusoHorario = dataAtual().toLocaleDateString("pt-br", nomeDoFusoHorario).slice(2);
-    document.getElementById("exibaHorarioTitle").innerHTML = `Horário ${pNomeDoFusoHorario} - ${DataHorarioHJ("hr")}`;
+    document.getElementById("exibirHorarioTitle").innerHTML = `Horário ${pNomeDoFusoHorario} - ${DataHorarioHJ("hr")}`;
+
+    if((dataAtual().getHours() == (5 || 12 || 17 || 23))){
+      if((dataAtual().getMinutes() == 59) && (dataAtual().getSeconds() == 59)){
+        InserirImagemDeFundo();
+      }
+    }
 
     const segundos = (dataAtual().getSeconds() / 60);
     const minutos = ((segundos + dataAtual().getMinutes()) / 60);
@@ -79,6 +123,7 @@ function CarregarRelogio() {
 
   // Executar
   InserirCSS();
+  InserirImagemDeFundo();
   NomeDoFusoHorario();
   InformarDataDeHJ();
   TempoDoRelogio();
