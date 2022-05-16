7f143e9ff89fa4e862c2ca8509c5cd2bc342641b
JG <diasusb@hotmail.com>
2022-05-16 11:44:07 -0300

    Add: funções para códigos que se repetem

--- relogiohd.js
+++ relogiohd.js
@@ -5,22 +5,23 @@ const semana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Qui
 
 function CarregarRelogio() {
   // Pegar Class do HTML
-  let selectClass = (element) => window.document.querySelectorAll(element);
+  let selectClass = (element="") => window.document.querySelectorAll(element);
   // Pegar os seletores do relógio (Hora, Minuto e Segundo)
-  let hora = window.document.querySelector("[data-ponteiro-hora]");
-  let minuto = window.document.querySelector("[data-ponteiro-minuto]");
-  let segundo = window.document.querySelector("[data-ponteiro-segundo]");
+  const milissegundo = 1000;
+  const hora = window.document.querySelector("[data-ponteiro-hora]");
+  const minuto = window.document.querySelector("[data-ponteiro-minuto]");
+  const segundo = window.document.querySelector("[data-ponteiro-segundo]");
   
   // Descobrir Data Atual
   const dataAtual = () => new Date();
 
-  // Descobrir o timeZoneName (Nome do Fuso Horário) //
+  // Descobrir o timeZoneName (Nome do Fuso Horário)
   const nomeDoFusoHorario = {
     day: '2-digit',
     timeZoneName: "long"
   };
 
-  // Formato para descobrir Data e Horario //
+  // Formato para descobrir Data e Horario
   const opcoesHorario = {
     timeZone: 'America/Sao_Paulo',
     hour: 'numeric',
@@ -29,87 +30,102 @@ function CarregarRelogio() {
   };
   const opcoesData = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-  // Inserir o CSS com JS
-  function InserirCSS() {
-    const styledRelogioHD = window.document.createElement("link");
-    styledRelogioHD.rel = "stylesheet"; styledRelogioHD.type = "text/css"; styledRelogioHD.href = "./relogiohd.css";
-    window.document.head.insertBefore(
-      styledRelogioHD, window.document.head.lastElementChild
-    );
+  // Formato designado para o nome do fuso horário
+  function formatoDoFusoHorario(tamanho="") {
+    nomeDoFusoHorario.timeZoneName = `${tamanho}`;
+    return dataAtual().toLocaleDateString("pt-br", nomeDoFusoHorario).slice(2);
+  }
+
+  // Mudar a aparência do site de acordo com o horário do dia
+  function EstiloDoDia(statusDoDia="", Autor="", Site="", link="") {
+    let imgBody = window.document.getElementById("body-bg");
+    let linkImgFundo = window.document.getElementById("linkImgFundo");
+    
+    imgBody.setAttribute("class", `${statusDoDia}`);
+    linkImgFundo.setAttribute("href", link);
+    linkImgFundo.textContent = `Image background created by ${Autor} - ${Site}`;
   }
 
   // Estilo da Tag SPAN
-  function EstiloBottomSpan(elemento01, elemento02) {
-    selectClass(elemento01).forEach(element => element.setAttribute("class", `${elemento02}`));
+  function EstiloBottomSpan(classInicial="", classFinal="") {
+    selectClass(classInicial).forEach(element => element.setAttribute("class", classFinal));
   }
-
+  
   // Imagem do Fundo da Página
   function InserirImagemDeFundo() {
     let hora = dataAtual().getHours();
-    let linkImgFundo = window.document.getElementById("linkImgFundo");
     let classExiste = !!document.getElementsByClassName("bottomSpanNoite");
-    let imgBody = window.document.getElementById("body-bg");
     if(hora < 6) {
-      imgBody.setAttribute("class", "madrugada");
+      EstiloDoDia("madrugada", "Daniel Chekalov", "Unsplash",
+        "https://unsplash.com/photos/OxU08SFhPbI"
+      );
       EstiloBottomSpan(".bottomSpanDia", "bottomSpanNoite");
-      linkImgFundo.setAttribute("href", "https://unsplash.com/photos/OxU08SFhPbI");
-      linkImgFundo.textContent = "Image background created by Daniel Chekalov - Unsplash";
     } else if(hora < 13) {
-      imgBody.setAttribute("class", "manha");
+      EstiloDoDia("manha", "Toa Heftiba", "Unsplash",
+        "https://unsplash.com/photos/bcLE7reXFLM"
+      );
       classExiste === true ? EstiloBottomSpan(".bottomSpanNoite", "bottomSpanDia") : "";
-      linkImgFundo.setAttribute("href", "https://unsplash.com/photos/bcLE7reXFLM");
-      linkImgFundo.textContent = "Image background created by Toa Heftiba - Unsplash";
     } else if(hora < 18) {
-      imgBody.setAttribute("class", "tarde");
+      EstiloDoDia("tarde", "aalmeidah", "Pixabay",
+        "https://pixabay.com/pt/illustrations/paisagem-ilustra%c3%a7%c3%a3o-outono-mulher-4572804/"
+      );
       classExiste === true ? EstiloBottomSpan(".bottomSpanNoite", "bottomSpanDia") : "";
-      linkImgFundo.setAttribute("href", "https://pixabay.com/pt/illustrations/paisagem-ilustra%c3%a7%c3%a3o-outono-mulher-4572804/");
-      linkImgFundo.textContent = "Image background created by aalmeidah - Pixabay";
     } else if(hora < 24) {
-      imgBody.setAttribute("class", "noite");
+      EstiloDoDia("noite", "Lili Kovac", "Unsplash",
+        "https://unsplash.com/photos/1SiXS0xQHTA"
+      );
       EstiloBottomSpan(".bottomSpanDia", "bottomSpanNoite");
-      linkImgFundo.setAttribute("href", "https://unsplash.com/photos/1SiXS0xQHTA");
-      linkImgFundo.textContent = "Image background created by Lili Kovac - Unsplash";
     } else {
-      window.document.body.style.backgroundColor = "#674A70";
+      window.document.body.style.backgroundColor = "#0d0d0e";
     }
   }
 
-  // Informar o Nome do Fuso Horário
+  // Informar o nome do fuso horário
   function NomeDoFusoHorario() {
-    let nomeFH = dataAtual().toLocaleDateString("pt-br", nomeDoFusoHorario).slice(2);
-    document.getElementById("nomeDoFusoHorario").innerHTML = `${nomeFH}`;
+    let lNomeDoFusoHorario = formatoDoFusoHorario("long");
+    document.getElementById("nomeDoFusoHorario").innerHTML = `${lNomeDoFusoHorario}`;
   }
 
-  // Descobrir Data e Horario //
-  function DataHorarioHJ(hoje) {
+  // Descobrir data e horário
+  function DataHorarioHJ(hoje="") {
     let formatoDeData = new Intl.DateTimeFormat([], opcoesHorario);
     let opcoesData = hoje === "datahj" ? dataAtual() : formatoDeData.format(dataAtual());
     return opcoesData;
   }
 
-  // Informar Data de Hoje
+  // Informar data de hoje
   function InformarDataDeHJ() {
     let infoData = (op) => DataHorarioHJ("datahj").toLocaleDateString("pt-br", op);
     let infoDataFormt = infoData(opcoesData);
     document.getElementById("dataHJ").textContent = `${infoDataFormt[0].toUpperCase()}${infoDataFormt.substring(1)}`;
   }
 
-  // Acrescendar 1 GRAU para cada segundo
-  function setRotacao(elemento, rRotacao) {
-    elemento.style.setProperty("--rotation", rRotacao * 360);
+  // Inserir horário na tag title
+  function InserirTextoNoTitle() {
+    const pNomeDoFusoHorario = formatoDoFusoHorario("short");
+    document.getElementById("exibirHorarioTitle").innerHTML = `Horário ${pNomeDoFusoHorario} - ${DataHorarioHJ("hr")}`;
   }
 
-  // Relogio para o Body e o Head 
-  function TempoDoRelogio() {
-    nomeDoFusoHorario.timeZoneName = "short";
-    let pNomeDoFusoHorario = dataAtual().toLocaleDateString("pt-br", nomeDoFusoHorario).slice(2);
-    document.getElementById("exibirHorarioTitle").innerHTML = `Horário ${pNomeDoFusoHorario} - ${DataHorarioHJ("hr")}`;
-
-    if((dataAtual().getHours() == (5 || 12 || 17 || 23))){
+  // Mudar automaticamente a imagem de fundo de acordo com o horário do dia
+  function statusDoDia() {
+    if((dataAtual().getHours() == 5) || (dataAtual().getHours() == 12) ||
+      (dataAtual().getHours() == 17) || (dataAtual().getHours() == 23)
+    ){
       if((dataAtual().getMinutes() == 59) && (dataAtual().getSeconds() == 59)){
         InserirImagemDeFundo();
       }
     }
+  }
+
+  // Acrescendar 1 grau para cada segundo
+  function setRotacao(elemento=any, rRotacao=0) {
+    elemento.style.setProperty("--rotation", rRotacao * 360);
+  }
+
+  // Relógio para o Body e o Head 
+  function TempoDoRelogio() {
+    InserirTextoNoTitle();
+    statusDoDia();
 
     const segundos = (dataAtual().getSeconds() / 60);
     const minutos = ((segundos + dataAtual().getMinutes()) / 60);
@@ -118,11 +134,11 @@ function CarregarRelogio() {
     setRotacao(minuto, minutos);
     setRotacao(hora, horas);
 
-    setTimeout(TempoDoRelogio, 1000);
+    setTimeout(TempoDoRelogio, milissegundo);
   }
 
   // Executar
-  InserirCSS();
+  InserirTextoNoTitle();
   InserirImagemDeFundo();
   NomeDoFusoHorario();
   InformarDataDeHJ();
