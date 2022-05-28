802916a7d17d4729517e5f3d1865b2c647717b1a
JG <diasusb@hotmail.com>
2022-05-27 22:06:00 -0300

    Add notificação para extensão

--- extension/chromium/background/background.js
+++ extension/chromium/background/background.js
@@ -16,6 +16,7 @@ chrome.runtime.onInstalled.addListener((details=Object) => {
     chrome.tabs.create({
       url: URL_INSTALL
     });
+    setNotifications(1, 5000, "notificationTitleInstall", "notificationContentInstall");
   } else if(details.reason === "update") {
     chrome.tabs.create({
       url: URL_UPDATE
@@ -23,11 +24,40 @@ chrome.runtime.onInstalled.addListener((details=Object) => {
   }
 });
 // --------------------------------------------------------------------------
-const MESSAGE = "desativado";
 
 chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
-  let id = chrome.runtime.id;
+  const MESSAGE = "desativado";
+  const STATUS_MESSAGE = "Extensão Pausada";
   if (request.message == MESSAGE) {
-    chrome.management.setEnabled(id, false); 
+    const id = chrome.runtime.id;
+    const tempo = 7000;
+    setNotifications(7, tempo, "notificationTitleOnPausar", "notificationContentOnPausar");
+    setTimeout(() => {
+      chrome.management.setEnabled(id, false); 
+    }, tempo);
   }
+  sendResponse({ status: STATUS_MESSAGE });
+  return true; 
 });
+
+function setNotifications(setId, setTime, setTitle, setContent) {
+  const id = `ID_Notifications_[${setId}]`;
+  const tempo = setTime;
+  const title = chrome.i18n.getMessage(setTitle);
+  const content = chrome.i18n.getMessage(setContent); 
+  const options = {
+    type: 'basic',
+    iconUrl: chrome.runtime.getURL("./src/relogio-icon-48x48.png"),
+    title: title,
+    message: content,
+    eventTime: Date.now(),
+    isClickable: false,
+    priority: 1
+  };
+  const createCallback = function(notificationId) { console.log(notificationId); };
+  chrome.notifications.create(id, options, createCallback); // returns 'ID_Notifications';
+  setTimeout(() => {
+    const clearCallback = function(notificationCleared) { console.log(notificationCleared); };
+    chrome.notifications.clear(id, clearCallback); // returns true;
+  }, tempo);
+}
--- extension/firefox/background/background.js
+++ extension/firefox/background/background.js
@@ -16,6 +16,7 @@ chrome.runtime.onInstalled.addListener((details=Object) => {
     chrome.tabs.create({
       url: URL_INSTALL
     });
+    setNotifications(1, 5000, "notificationTitleInstall", "notificationContentInstall");
   } else if(details.reason === "update") {
     chrome.tabs.create({
       url: URL_UPDATE
@@ -27,7 +28,28 @@ const MESSAGE = "desativado";
 
 browser.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
   if (request.message == MESSAGE) {
-    let id = browser.runtime.id;
-    browser.management.setEnabled(id, false); 
+    setNotifications(7, 7000, "notificationTitleOffPausar", "notificationContentOffPausar");
   }
 });
+
+function setNotifications(setId, setTime, setTitle, setContent) {
+  const id = `ID_Notifications_[${setId}]`;
+  const tempo = setTime;
+  const title = browser.i18n.getMessage(setTitle);
+  const content = browser.i18n.getMessage(setContent); 
+  const options = {
+    type: 'basic',
+    iconUrl: browser.extension.getURL("./src/relogio-icon-48x48.png"),
+    title: title,
+    message: content,
+    eventTime: Date.now(),
+    isClickable: false,
+    priority: 1
+  };
+  const createCallback = function(notificationId) { console.log(notificationId); };
+  browser.notifications.create(id, options, createCallback); // returns 'ID_Notifications';
+  setTimeout(() => {
+    const clearCallback = function(notificationCleared) { console.log(notificationCleared); };
+    browser.notifications.clear(id, clearCallback); // returns true;
+  }, tempo);
+}
